e3cc969a440b391e17e5199effce2ceb0ca498a7
manoel-lopes <manoel.lopes.full@gmail.com>
2024-12-12 19:37:27 -0300

    fix: enhance environment variable validation and error reporting

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -9,14 +9,24 @@ const _env = z
       .string()
       .default('3000')
       .transform((port) => Number(port)),
+    DB_USER: z.string(),
+    DB_PASSWORD: z.string(),
+    DB_HOST: z.string(),
+    DB_PORT: z.string(),
+    DB_NAME: z.string(),
   })
   .safeParse(process.env)
 
 if (!_env.success) {
-  console.error(
-    `\n\x1b[1m\x1b[31m❌ Invalid environment variables:', _env.error.format()\x1b[0m\n`,
-    _env.error.format(),
-  )
+  const formattedErrors = Object.entries(_env.error.format())
+    .filter(([key, value]) => key !== '_errors' && value && '_errors' in value)
+    .map(([key]) => `${key} is required`)
+
+  console.error('\n\x1b[1m\x1b[31m❌ Invalid environment variables:\x1b[0m')
+  for (const error of formattedErrors) {
+    console.error(`- ${error}`)
+  }
+
   process.exit(1)
 }
 
