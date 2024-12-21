d16ffbdba6b11e5743a40f9550d520866eb14093
manoel-lopes <manoel.lopes.full@gmail.com>
2024-12-21 11:51:48 -0300

    chore: refine entry patterns in tsup configuration to exclude specific directories and file types

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/**/*.ts', '!src/**/*.test.ts'],
+  entry: [
+    'src/**/*.ts',
+    '!src/core/*/*.ts',
+    '!src/**/*.{test, spec}.ts',
+    '!src/application/repositories/*.ts',
+    '!src/**/{models,ports,stub,in-memory,types}/**/*.ts',
+  ],
   clean: true,
 })
