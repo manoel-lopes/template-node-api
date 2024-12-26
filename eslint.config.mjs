491b533b05238c957f5acb93ba70cd8d43a832c6
manoel-lopes <manoel.lopes.full@gmail.com>
2024-12-26 16:33:49 -0300

    removing uncessecary 'noUnusedVarsConfig' variable

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,6 @@ import tseslint from 'typescript-eslint'
 import neostandard, { resolveIgnoresFromGitignore } from 'neostandard'
 import vitest from '@vitest/eslint-plugin'
 
-const noUnusedVarsConfig = ['error',
-  {
-    argsIgnorePattern: '^_',
-    varsIgnorePattern: '^_',
-  }]
-
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
@@ -27,7 +21,7 @@ export default [
     rules: {
       'no-useless-constructor': 'off',
       'no-var': 'error',
-      'no-unused-vars': noUnusedVarsConfig,
+      'no-unused-vars': 'off',
       '@stylistic/max-len': ['warn', {
         code: 98,
         tabWidth: 2,
@@ -44,7 +38,10 @@ export default [
       '@stylistic/comma-dangle': ['error', 'always-multiline'],
       '@stylistic/multiline-ternary': ['error', 'always'],
       '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-unused-vars': noUnusedVarsConfig,
+      '@typescript-eslint/no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      }],
     },
   },
 ]
