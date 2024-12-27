0dfff368835ba3976484fddc20984cc2e4219eca
manoel-lopes <manoel.lopes.full@gmail.com>
2024-12-27 13:51:37 -0300

    feat: add delete method to BaseRepository type

--- src/core/application/base-repository.ts
+++ src/core/application/base-repository.ts
@@ -1,4 +1,5 @@
 export type BaseRepository<Entity> = {
   save: (entity: Entity) => Promise<void>
   getById(entityId: string): Promise<Entity | null>
+  delete(entityId: string): Promise<Entity | null>
 }
