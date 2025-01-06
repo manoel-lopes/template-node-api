69bb5893946bb4f85e0a5a6f8ab7d0b52903c9ed
manoel-lopes <manoel.lopes.full@gmail.com>
2025-01-06 11:25:35 -0300

    refactor: update tsup config to exclude additional directories from entry

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: [
     'src/**/*.ts',
-    '!src/core/application/*.ts',
+    '!src/core/{application,presentation}/*.ts',
     '!src/**/*.{test, spec}.ts',
     '!src/application/repositories/*.ts',
+    '!src/util/factories/*.ts',
     '!src/**/{models,ports,stub,in-memory,types}/**/*.ts',
   ],
   clean: true,
