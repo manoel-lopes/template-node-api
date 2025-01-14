1975bacb78da3ac9cc5c58189c39be5c0d705069
manoel-lopes <manoel.lopes.full@gmail.com>
2025-01-14 11:03:59 -0300

    refactor: error handling to return correct status codes for schema parse errors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -9,10 +9,10 @@ app.setErrorHandler((error, _, res) => {
   if (error instanceof SchemaParseFailedError) {
     const isRequiredError = error.message.includes('required')
     if (isRequiredError) {
-      return res.status(404).json({ error: error.message })
+      return res.status(400).json({ error: error.message })
     }
 
-    return res.status(400).json({ error: error.message })
+    return res.status(422).json({ error: error.message })
   }
 
   if (env.NODE_ENV !== 'production') {
