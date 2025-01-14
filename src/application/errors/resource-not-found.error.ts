3659d1cd27ca68dc2c57ee3a930f0071a3e2a46d
manoel-lopes <manoel.lopes.full@gmail.com>
2025-01-14 15:04:45 -0300

    refactor: remove ResourceNotFoundError and related error handling from app configuration

--- src/application/errors/resource-not-found.error.ts
+++
@@ -1,8 +0,0 @@
-type Resource = 'Question' | 'Answer' | 'User'
-
-export class ResourceNotFoundError extends Error {
-  constructor(resource: Resource) {
-    super(`${resource} not found`)
-    this.name = 'ResourceNotFoundError'
-  }
-}
