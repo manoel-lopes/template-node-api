057243b7d544acd849478b2f90d6c6154cd4fffe
manoel-lopes <manoel.lopes.full@gmail.com>
2025-01-14 08:40:03 -0300

    feat: add HTTP status code 401 and 422 to HttpStatusCode type

--- src/infra/adapters/http/ports/http-server.ts
+++ src/infra/adapters/http/ports/http-server.ts
@@ -1,14 +1,19 @@
-import type { Middleware, ListenOptions, RouteOptions, ErrorHandler } from '@/infra/api/ports'
+import type {
+  Middleware,
+  RouteOptions,
+  ListenOptions,
+  ErrorHandler,
+} from '@/infra/api/ports'
 
 export type HttpServer = {
-  use(middleware: Middleware): void
-  use(middleware: Middleware): void
   get(url: string, options: RouteOptions, ...handlers: Middleware[]): void
   post(url: string, options: RouteOptions, ...handlers: Middleware[]): void
   put(url: string, options: RouteOptions, ...handlers: Middleware[]): void
   patch(url: string, options: RouteOptions, ...handlers: Middleware[]): void
   delete(url: string, options: RouteOptions, ...handlers: Middleware[]): void
-  setErrorHandler(errorHandler: ErrorHandler): void
   listen(options?: ListenOptions): Promise<void>
   close(): Promise<void>
+  setErrorHandler(errorHandler: ErrorHandler): void
+  use(middleware: Middleware): void
 }
+
--- src/infra/http/ports/index.ts
+++ src/infra/http/ports/index.ts
@@ -1,6 +1,6 @@
 export type HttpMethod = 'POST' | 'GET' | 'PUT' | 'PATCH' | 'DELETE'
 
-export type HttpStatusCode = 200 | 201 | 400 | 403 | 404 | 409 | 500
+export type HttpStatusCode = 200 | 201 | 400 | 401 | 403 | 404 | 409 | 422 | 500
 
 export type HttpRequest = {
   body?: unknown
