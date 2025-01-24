e8285e34a2856301283231299c5135dcbe6d8996
manoel-lopes <manoel.lopes.full@gmail.com>
2025-01-24 10:29:25 -0300

    refactor: enhance error handling in SchemaParser and improve message formatting

--- src/external/schemas/zod/helpers/schema-parser.ts
+++ src/external/schemas/zod/helpers/schema-parser.ts
@@ -1,34 +1,38 @@
 import { z } from 'zod'
-import {
-  SchemaValidationError,
-} from '@/infra/adapters/validation/errors/schema-validation.error'
+import type { SchemaResult } from '@/infra/adapters/validation/schemas/ports/schema-validator'
+import { SchemaValidationError } from '@/infra/adapters/validation/errors/schema-validation.error'
 
 export abstract class SchemaParser {
-  static parse<T>(schema: z.Schema, data: unknown): T {
+  static parse<T = SchemaResult>(schema: z.Schema, data: unknown): T {
     const parsedSchema = schema.safeParse(data)
-    const { error } = parsedSchema
-    if (error) {
-      throw new SchemaValidationError(this.formatValidationErrorMessage(
-        this.makeSchemaValidationErrorMessage(error.errors[0]),
-      ))
+    if (!parsedSchema.success) {
+      const errorMessage = this.formatValidationErrorMessage(
+        this.makeSchemaValidationErrorMessage(parsedSchema.error.errors[0]),
+      )
+      throw new SchemaValidationError(errorMessage)
     }
     return parsedSchema.data
   }
 
-  private static makeSchemaValidationErrorMessage(issues: z.ZodIssue) {
-    const { path, message } = issues
+  private static makeSchemaValidationErrorMessage(issue: z.ZodIssue): string {
+    const { path, message } = issue
+    const field = path[0]
     const msg = message.toLowerCase()
-    const isObjectFieldError = path.length === 3
-    if (!isObjectFieldError) {
-      const field = path[0]
-      return `Field '${field}' ${msg === 'required' ? `is ${msg}` : msg}`
+    if (path.length === 1) {
+      return this.getFieldErrorMessage(String(field), msg)
     }
-
-    const [_, object, field] = path
-    return `${object} ${field} ${msg}`
+    const [_, object, subField] = path
+    return `${object} ${subField} ${msg}`
   }
 
-  private static formatValidationErrorMessage(message: string) {
+  private static getFieldErrorMessage(field: string, msg: string): string {
+    if (msg.includes('invalid')) {
+      return `Invalid ${field}`
+    }
+    return `Field '${field}' ${msg === 'required' ? `is ${msg}` : msg}`
+  }
+
+  private static formatValidationErrorMessage(message: string): string {
     return message.replace(/(\d+)\scharacter\(s\)/g, (_, num) => {
       return `${num} character${num > 1 ? 's' : ''}`
     })
