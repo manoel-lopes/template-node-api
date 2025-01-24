f1f094474be06b50f4339a4070fdde6cdef8a187
manoel-lopes <manoel.lopes.full@gmail.com>
2025-01-24 12:53:59 -0300

    refactor: simplify Body, Paramd and Query htpt types

--- src/infra/http/ports/index.ts
+++ src/infra/http/ports/index.ts
@@ -2,9 +2,9 @@ export type HttpMethod = 'POST' | 'GET' | 'PUT' | 'PATCH' | 'DELETE'
 
 export type HttpStatusCode = 200 | 201 | 400 | 401 | 403 | 404 | 409 | 422 | 500
 
-type Body = Record<string, unknown>
-type Params = Record<string, unknown>
-type Query = Record<string, unknown>
+type Body = unknowwn
+type Params = unknown
+type Query = unknown
 
 export type HttpRequest = {
   body?: Body
