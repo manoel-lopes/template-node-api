2c732e1f34a76adf1d3fb5e81192bebd77478b43
manoel-lopes <manoel.lopes.full@gmail.com>
2025-01-26 16:03:25 -0300

    feat: add router adapter utility function

---
+++ src/util/adapt-route.ts
@@ -0,0 +1,9 @@
+import type { WebController } from '@/core/presentation/web-controller'
+import type { ApiRequest, ApiResponse } from '@/infra/adapters/http/ports/http-server'
+
+export function adaptRoute(controller: WebController) {
+  return async (req: ApiRequest, res: ApiResponse) => {
+    const { statusCode, body } = await controller.handle(req)
+    res.status(statusCode).json(body)
+  }
+}
