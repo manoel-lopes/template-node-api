67feeaa4af029895801ef94ef7593b25deb0668d
manoel-lopes <manoel.lopes.full@gmail.com>
2025-01-26 20:14:58 -0300

    refactor(core): removing 'delete' and 'getById' methods from base repository type

--- src/core/application/base-repository.ts
+++ src/core/application/base-repository.ts
@@ -1,5 +1,3 @@
 export type BaseRepository<Entity> = {
   save: (entity: Entity) => Promise<void>
-  getById(entityId: string): Promise<Entity | null>
-  delete(entityId: string): Promise<Entity | null>
 }
