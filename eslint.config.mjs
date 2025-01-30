bd76835987e1e17176174b5287133f0fff5150e2
manoel-lopes <manoel.lopes.full@gmail.com>
2025-01-30 00:53:24 -0300

    config: increase max line length in ESLint configuration to improve code readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,7 +24,7 @@ export default [
       'no-unused-vars': 'off',
       'no-console': ['error', { allow: ['error'] }],
       '@stylistic/max-len': ['warn', {
-        code: 85,
+        code: 99,
         tabWidth: 2,
         ignoreUrls: true,
         ignoreComments: false,
