56202b7d0c439aff0947ad617e977222e37de37e
manoel-lopes <manoel.lopes.full@gmail.com>
2025-02-08 22:50:53 -0300

    refactor(cache): remove old RedisProvider and implement new structure with delete method

--- src/infra/adapters/providers/cache-service/ports/cache-service.provider.ts
+++
@@ -1,4 +0,0 @@
-export type CacheServiceProvider = {
-  get: (key: string) => Promise<string | null>
-  set: (key: string, value: string) => Promise<void>
-}
---
+++ src/infra/providers/cache/redis/redis.provider.ts
@@ -0,0 +1,17 @@
+import Redis from 'ioredis'
+import type { CacheService } from '../ports/cache-service'
+
+export class RedisProvider implements CacheService {
+  private readonly client = new Redis()
+  async set(key: string, value: string): Promise<void> {
+    this.client.set(key, value)
+  }
+
+  async get(key: string): Promise<string | null> {
+    return this.client.get(key)
+  }
+
+  async delete(key: string): Promise<void> {
+    this.client.del(key)
+  }
+}
