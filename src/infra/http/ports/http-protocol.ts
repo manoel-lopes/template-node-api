40a05063427ef11c164819cf8596888d295556fd
manoel-lopes <manoel.lopes.full@gmail.com>
2025-02-12 20:19:19 -0300

    refactor: enhance HttpRequest and HttpResponse types for better type safety

--- src/infra/http/ports/http-protocol.ts
+++ src/infra/http/ports/http-protocol.ts
@@ -2,14 +2,16 @@ export type HttpMethod = 'POST' | 'GET' | 'PUT' | 'PATCH' | 'DELETE'
 
 export type HttpStatusCode = 200 | 201 | 400 | 401 | 403 | 404 | 409 | 422 | 500
 
-export type HttpRequest<Body = unknown, Params = unknown, Query = unknown> = {
+export type BodyParams = Record<string, unknown>
+export type RouteParams = Record<string, string>
+export type QueryParams = unknown
+export type HttpRequest<Body = BodyParams, Params = RouteParams, Query = QueryParams> = {
   body?: Body
   params?: Params
   query?: Query
 }
 
-export type HttpResponse = {
+export type HttpResponse<Body = unknown> = {
   statusCode: HttpStatusCode
-  body?: unknown
+  body?: Body
 }
-
