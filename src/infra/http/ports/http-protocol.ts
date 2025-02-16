8d5901f40bd1cabe8b3d2ebf00be1d5d2666bacd
manoel-lopes <manoel.lopes.full@gmail.com>
2025-02-16 12:55:32 -0300

    refactor: enforce required properties in HttpRequest type for improved type safety

--- src/infra/http/ports/http-protocol.ts
+++ src/infra/http/ports/http-protocol.ts
@@ -5,10 +5,13 @@ export type HttpStatusCode = 200 | 201 | 400 | 401 | 403 | 404 | 409 | 422 | 500
 export type BodyParams = Record<string, unknown>
 export type RouteParams = Record<string, string>
 export type QueryParams = unknown
-export type HttpRequest<Body = BodyParams, Params = RouteParams, Query = QueryParams> = {
-  body?: Body
-  params?: Params
-  query?: Query
+export type HttpRequest<
+Body = BodyParams | undefined,
+Params = RouteParams | undefined,
+Query = QueryParams | undefined> = {
+  body: Body
+  params: Params
+  query: Query
 }
 
 export type HttpResponse<Body = unknown> = {
