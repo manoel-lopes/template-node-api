813e10ee6730a8c8160885d0cdaf52305f15d1e4
manoel-lopes <manoel.lopes.full@gmail.com>
2025-02-19 14:38:51 -0300

    config: change vitest configuration to enhance test exclusion patterns and coverage settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import path from 'node:path'
 import { defineConfig, configDefaults } from 'vitest/config'
 
+const exclude = [
+  ...configDefaults.exclude,
+  'src/main',
+]
+
 export default defineConfig({
   resolve: {
     alias: {
@@ -8,10 +13,25 @@ export default defineConfig({
     },
   },
   test: {
-    exclude: [...configDefaults.exclude, 'src/main'],
+    exclude,
+    globals: true,
+    coverage: {
+      provider: 'istanbul',
+      exclude: [
+        ...exclude,
+        'src/lib',
+        'src/util',
+        'src/external',
+        'src/infra/adapters/http/http-server/fasitfy',
+        'src/infra/persistence/repositories/cache',
+        'src/infra/validation/schemas/zod',
+        'src/infra/providers/cache/redis',
+        'src/infra/persistence/db',
+      ],
+    },
   },
 })
 
-function resolve(dir: string) {
+function resolve (dir) {
   return path.join(__dirname, dir)
 }
