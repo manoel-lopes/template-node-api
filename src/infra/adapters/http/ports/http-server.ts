84e320e818eb93dcabd5dcba89f3be3d05dd9f90
manoel-lopes <manoel.lopes.full@gmail.com>
2025-02-19 16:17:23 -0300

    fix: remove unused HttpRequest import in http-server adapter

--- src/infra/adapters/http/ports/http-server.ts
+++ src/infra/adapters/http/ports/http-server.ts
@@ -1,4 +1,4 @@
-import type { HttpRequest, HttpStatusCode } from '@/infra/http/ports/http-protocol'
+import type { HttpStatusCode } from '@/infra/http/ports/http-protocol'
 
 export type ApiRequest = {
   body?: unknown
