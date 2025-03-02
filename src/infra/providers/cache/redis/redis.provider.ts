495f4dcb07431cfb18457521c6bd2d90fffcc9e8
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-02 13:52:31 -0300

    fix: ensure RedisProvider methods await asynchronous operations

--- src/infra/providers/cache/redis/redis.provider.ts
+++ src/infra/providers/cache/redis/redis.provider.ts
@@ -4,14 +4,14 @@ import type { CacheService } from '../ports/cache-service'
 export class RedisProvider implements CacheService {
   private readonly client = new Redis()
   async set (key: string, value: string): Promise<void> {
-    this.client.set(key, value)
+    await this.client.set(key, value)
   }
 
   async get (key: string): Promise<string | null> {
-    return this.client.get(key)
+    return await this.client.get(key)
   }
 
   async delete (key: string): Promise<void> {
-    this.client.del(key)
+    await this.client.del(key)
   }
 }
