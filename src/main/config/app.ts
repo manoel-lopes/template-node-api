29cbfe2ef1b231c10c214565a7b422edf45bd83b
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-03 17:51:50 -0300

    refactor: remove route configuration file and setup

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,6 @@
 import { FastifyAdapter } from '@/infra/adapters/http/http-server/fasitfy/fasitfy.adapter'
 import { FallbackController } from '@/infra/http/fallback/fallback.controller'
-import { setRoutes } from './routes'
 
 const app = new FastifyAdapter()
-setRoutes(app)
 app.setErrorHandler(FallbackController.handle)
 export { app }
--- src/main/config/routes.ts
+++
@@ -1,7 +0,0 @@
-import type { HttpServer } from '@/infra/adapters/http/ports/http-server'
-
-export function setRoutes (app: HttpServer) {
-  app.get('/', {}, async (_, res) => {
-    res.status(200).json('Hello World!')
-  })
-}
