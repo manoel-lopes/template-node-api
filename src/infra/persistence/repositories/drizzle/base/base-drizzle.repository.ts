ae58b3199dc16351b348ef13766f639cab6ecbd6
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-07 19:48:51 -0300

    feat: add findOne method to BaseDrizzleRepository for retrieving a single entity

--- src/infra/persistence/repositories/drizzle/base/base-drizzle.repository.ts
+++ src/infra/persistence/repositories/drizzle/base/base-drizzle.repository.ts
@@ -1,6 +1,11 @@
-import { type InferInsertModel, type InferSelectModel, sql } from 'drizzle-orm'
-import type { PgTable } from 'drizzle-orm/pg-core'
+import type { PgTable, AnyPgColumn } from 'drizzle-orm/pg-core'
 import { db } from '@/infra/persistence/drizzle/client'
+import { and, eq, sql, type InferInsertModel, type InferSelectModel } from 'drizzle-orm'
+
+export type FindOptions<Table extends PgTable> = {
+  where: Partial<Record<
+  keyof InferSelectModel<Table>, InferSelectModel<Table>[keyof InferSelectModel<Table>]>>
+}
 
 export abstract class BaseDrizzleRepository<Table extends PgTable> {
   constructor (private readonly table: Table) {}
@@ -11,5 +16,19 @@ export abstract class BaseDrizzleRepository<Table extends PgTable> {
 
   async findAll (): Promise<InferSelectModel<Table>[]> {
     return db.select().from(sql`${this.table}`).execute() as Promise<InferSelectModel<Table>[]>
+  }
+
+  async findOne ({ where }: FindOptions<Table>): Promise<InferSelectModel<Table> | null> {
+    const whereClause = Object.entries(where).map(([key, value]) => {
+      return eq(this.table[key as keyof Table] as AnyPgColumn, value)
+    })
+
+    const result = await db.select()
+      .from(sql`${this.table}`)
+      .where(and(...whereClause))
+      .limit(1)
+      .execute() as InferSelectModel<Table>[]
+
+    return result.length > 0 ? result[0] : null
   }
 }
