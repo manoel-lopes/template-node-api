a0234ec9911720af910d70f916cfab2134371f31
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-07 18:54:37 -0300

    refactor: change BaseDrizzleRepository to an abstract class and simplify constructor

--- src/infra/persistence/repositories/drizzle/base/base-drizzle.repository.ts
+++ src/infra/persistence/repositories/drizzle/base/base-drizzle.repository.ts
@@ -2,12 +2,8 @@ import { db } from '@/infra/persistence/drizzle/client'
 import type { InferInsertModel } from 'drizzle-orm'
 import type { PgTable } from 'drizzle-orm/pg-core'
 
-export class BaseDrizzleRepository<Table extends PgTable> {
-  private table: Table
-
-  constructor (table: Table) {
-    this.table = table
-  }
+export abstract class BaseDrizzleRepository<Table extends PgTable> {
+  constructor (private table: Table) {}
 
   async save (entity: InferInsertModel<Table>): Promise<void> {
     await db.insert(this.table).values(entity)
