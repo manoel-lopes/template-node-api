e6ef98f11c9a60f2fecf303cf3be43d978724416
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-07 19:31:08 -0300

    refactor: findAll method to return typed results

--- src/infra/persistence/repositories/drizzle/base/base-drizzle.repository.ts
+++ src/infra/persistence/repositories/drizzle/base/base-drizzle.repository.ts
@@ -1,15 +1,15 @@
-import { sql, type InferInsertModel } from 'drizzle-orm'
+import { type InferInsertModel, type InferSelectModel, sql } from 'drizzle-orm'
 import type { PgTable } from 'drizzle-orm/pg-core'
 import { db } from '@/infra/persistence/drizzle/client'
 
 export abstract class BaseDrizzleRepository<Table extends PgTable> {
-  constructor (private table: Table) {}
+  constructor (private readonly table: Table) {}
 
   async save (entity: InferInsertModel<Table>): Promise<void> {
     await db.insert(this.table).values(entity)
   }
 
-  async findAll () {
-    return db.select().from(sql`${this.table}`)
+  async findAll (): Promise<InferSelectModel<Table>[]> {
+    return db.select().from(sql`${this.table}`).execute() as Promise<InferSelectModel<Table>[]>
   }
 }
