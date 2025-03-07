586853921f874aebfb3d2118ee482e69b5f47756
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-07 12:21:02 -0300

    refactor: remove Drizzle ORM configuration and related files

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'drizzle-kit'
-import { dbCredentials } from './src/infra/persistence/drizzle/client'
-
-export default defineConfig({
-  dialect: 'postgresql',
-  schema: 'src/infra/persistence/drizzle/schemas/**.schema.ts',
-  out: 'src/infra/persistence/drizzle/migrations',
-  dbCredentials,
-})
