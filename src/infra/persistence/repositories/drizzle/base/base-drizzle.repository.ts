167abd6e93d037d56bbb367456053a2a7e9bacb5
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-20 16:30:06 -0300

    refactor: change FindWeirdOptions to use Partial for improved flexibility

--- src/infra/persistence/repositories/drizzle/base/base-drizzle.repository.ts
+++ src/infra/persistence/repositories/drizzle/base/base-drizzle.repository.ts
@@ -1,49 +1,52 @@
-import type { PgTable, AnyPgColumn } from 'drizzle-orm/pg-core'
-import { and, eq, type InferInsertModel, type InferSelectModel } from 'drizzle-orm'
 import { db } from '@/infra/persistence/drizzle/client'
+import { type InferInsertModel, type InferSelectModel, and, eq } from 'drizzle-orm'
+import type { AnyPgColumn, PgTable } from 'drizzle-orm/pg-core'
+
+type FindWeirdOptions<Table extends PgTable> = Partial<
+  Record<keyof InferSelectModel<Table>, InferSelectModel<Table>[keyof InferSelectModel<Table>]>
+>
 
 export type FindOptions<Table extends PgTable> = {
-  where: Partial<Record<
-  keyof InferSelectModel<Table>, InferSelectModel<Table>[keyof InferSelectModel<Table>]>>
+  where: FindWeirdOptions<Table>
 }
 
 export abstract class BaseDrizzleRepository<Table extends PgTable> {
-  constructor (private readonly table: Table) {}
+  constructor(private readonly table: Table) {}
 
-  async save (entity: InferInsertModel<Table>): Promise<void> {
+  async save(entity: InferInsertModel<Table>): Promise<void> {
     await db.insert(this.table).values(entity)
   }
 
-  async findOne ({ where }: FindOptions<Table>): Promise<InferSelectModel<Table> | null> {
-    const whereClause = this.buildWhereClause(where)
-    const [entity] = await db.select()
+  async findOne({ where }: FindOptions<Table>): Promise<InferSelectModel<Table> | null> {
+    const [entity] = (await db
+      .select()
       .from(this.table as PgTable)
-      .where(and(...whereClause))
-      .limit(1) as InferSelectModel<Table>[]
+      .where(and(...this.buildWhereClause(where)))
+      .limit(1)) as InferSelectModel<Table>[]
 
     return entity ?? null
   }
 
-  async deleteOne ({ where }: FindOptions<Table>): Promise<void> {
-    const whereClause = this.buildWhereClause(where)
-    await db.delete(this.table).where(and(...whereClause))
+  async deleteOne({ where }: FindOptions<Table>): Promise<void> {
+    await db.delete(this.table).where(and(...this.buildWhereClause(where)))
   }
 
-  async updateOne (
+  async updateOne(
     { where }: FindOptions<Table>,
     data: Partial<InferInsertModel<Table>>
   ): Promise<InferSelectModel<Table>> {
-    const whereClause = this.buildWhereClause(where)
-    const [updatedEntity] = await db.update(this.table)
+    const [updatedEntity] = (await db
+      .update(this.table)
       .set({ ...data })
-      .where(and(...whereClause))
-      .returning() as InferSelectModel<Table>[]
+      .where(and(...this.buildWhereClause(where)))
+      .returning()) as InferSelectModel<Table>[]
 
     return updatedEntity
   }
 
-  private buildWhereClause (where: FindOptions<Table>['where']) {
+  private buildWhereClause(where: FindOptions<Table>['where']) {
     return Object.entries(where).map(([key, value]) =>
-      eq(this.table[key as keyof Table] as AnyPgColumn, value))
+      eq(this.table[key as keyof Table] as AnyPgColumn, value)
+    )
   }
 }
