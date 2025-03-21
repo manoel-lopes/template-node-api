e7a92924d377cd03191d7168928de8932df36597
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-21 18:18:01 -0300

    refactor: FastifyAdapter to use custom validation compiler

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import { FastifyAdapter } from '@/infra/adapters/http/http-server/fasitfy/fasitfy.adapter'
 import { FallbackController } from '@/infra/http/fallback/fallback.controller'
+import { ZodSchemaParser } from '@/external/zod/helpers/zod-schema-parser'
 import { env } from '@/lib/env'
 
 const app = new FastifyAdapter({ logger: env.NODE_ENV !== 'production' })
 app.setErrorHandler(FallbackController.handle)
+app.setValidationCompiler(ZodSchemaParser.parse)
 export { app }
