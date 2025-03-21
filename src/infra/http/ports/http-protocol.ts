4a5e1c82c2490df918b28f730429f34848a95516
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-21 18:26:04 -0300

    refactor: allow any type for HttpRequest body, params, and query

--- src/infra/http/ports/http-protocol.ts
+++ src/infra/http/ports/http-protocol.ts
@@ -4,10 +4,8 @@ export type HttpStatusCode = 200 | 201 | 400 | 401 | 403 | 404 | 409 | 422 | 500
 
 export type HttpRedirectStatusCode = 301 | 302
 
-export type HttpRequest<
-Body = unknown,
-Params = unknown,
-Query = unknown> = {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type HttpRequest<Body = any, Params = any, Query = any> = {
   body?: Body
   params?: Params
   query?: Query
